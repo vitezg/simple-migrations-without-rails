f204890b429cc8a7758a0b7b220fb9d1fb5cc65b
Your Name <gabor.vitez@gmail.com>
2012-08-13 15:24:49 +0200

    syntax fix

--- src/migratedb.rb
+++ src/migratedb.rb
@@ -10,5 +10,5 @@ ActiveRecord::Migration.verbose = true
 
 #migrations should be placed in the "migrations" subdirectory
 #the script's argv[0] should be the desired version to migrate to
-#if there is no argument, this will migrate to the highes version
+#if there is no argument, this will migrate to the highest version
 ActiveRecord::Migrator.migrate "migrations/", ARGV[0] ? ARGV[0].to_i : nil
